5000237dabd80dddea1255fbd2d58003191e0a94
Dali Hoffman (11005824) <HOFFMDE1@student.op.ac.nz>
2022-05-25 16:33:30 +1200

    just added the updated stuff

--- js/weather.js
+++ js/weather.js
@@ -4,10 +4,15 @@ const OPEN_URL = "api.openweathermap.org";
 const OPEN_KEY = "a0734a6c8412a878935845b45f197bd6";
 
 let body = document.querySelector("body");
+let grid1 = document.querySelector("grid4");
+
 
 fetch("https://api.openweathermap.org/data/2.5/weather?q=Ravensbourne,nz&APPID=a0734a6c8412a878935845b45f197bd6")
 .then(res => res.json())
 .then(d => { 
     let wind = d["wind"];
+    let div = document.createElement("div");
+
+
     console.log(wind);
 });
