7ea77a04c383d844293e76001e71a68fdc11184c
Dali Hoffman (11005824) <HOFFMDE1@student.op.ac.nz>
2022-05-25 17:05:12 +1200

    updated the background images to be working

--- js/weather.js
+++ js/weather.js
@@ -4,15 +4,26 @@ const OPEN_URL = "api.openweathermap.org";
 const OPEN_KEY = "a0734a6c8412a878935845b45f197bd6";
 
 let body = document.querySelector("body");
-let grid1 = document.querySelector("grid4");
+let grid4 = document.querySelector("grid4");
+
+
 
 
 fetch("https://api.openweathermap.org/data/2.5/weather?q=Ravensbourne,nz&APPID=a0734a6c8412a878935845b45f197bd6")
 .then(res => res.json())
 .then(d => { 
     let wind = d["wind"];
-    let div = document.createElement("div");
+    
+    let titles = ["Weather", "Feels like", "Wind Speed", "Wind Degrees"];
+    for (let index = 0; index < titles.length; index++) {
+        let div = document.createElement("div");
+        div.innerHTML = titles[index];
+        grid4.append(div);
+    }
 
+    
 
-    console.log(wind);
+// when you want to be a server
+
+    
 });
