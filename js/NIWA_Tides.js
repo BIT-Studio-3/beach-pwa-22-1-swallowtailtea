2eee7e27a98f0da8a32027fbfa906d74f0f813de
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2022-05-27 23:27:23 +1200

    add location info to separate js file

--- js/NIWA_Tides.js
+++ js/NIWA_Tides.js
@@ -1,4 +1,7 @@
-// Query parameters: lat + long + numberOfDays (default 7) + startdate (default: today) + datum (default lowest astronomical tide) + NIWA key
+// Query parameters: lat + long + numberOfDays (default 7) + startdate (default: today) + datum (default: LAT) + NIWA key
+// Note: NIWA key will not work for serving locally with 11ty. Omit NIWA key from queries if serving locally.
+// Please see locations.js for the available locations 
+
 const NIWA_URL = "https://api.niwa.co.nz/tides/"
 const NIWA_KEY = "&apikey=F8g5zg4vEey0dOJriP6XxU5o1RVbgeW3";
 const NIWA_PATHS = {
@@ -7,15 +10,6 @@ const NIWA_PATHS = {
     data: "data?",
     data_csv: "data.csv?"
 };
-
-// these are the locations requested by the client
-let marina = buildLocationObject("Marina", buildLatLong(-45.87161, 170.52805), buildLatLong(-45.87323533,170.52796092));
-let vauxhall = buildLocationObject("Vauxhall Yacht Club", buildLatLong(-45.88478, 170.52489), buildLatLong(-45.88640507,170.52480075));
-let portChalmers = buildLocationObject("Port Chalmers", buildLatLong(-45.81590, 170.62135), buildLatLong(-45.81752693,170.62126097));
-let broadBay = buildLocationObject("Broad Bay Boating", buildLatLong(-45.848117,170.620410), buildLatLong(-45.84974329,170.62032050));
-let tairoaHead = buildLocationObject("Tairoa Head", buildLatLong(-45.77281,170.72846), buildLatLong(-45.77443858,170.72837034));
-
-let locations = [marina, vauxhall, portChalmers, broadBay, tairoaHead];
 
 // format a date for the NIWA Tides API
 function formatDate(myDate)
@@ -27,23 +21,6 @@ function formatDate(myDate)
     }
     let formattedDate = `${myDate.getFullYear()}-${month}-${myDate.getDate()}`
     return formattedDate;
-}
-
-// return an object containing latitude and longitude
-function buildLatLong(lat, long)
-{
-    return {latitude: lat, longitude: long};
-}
-
-// return an object representing a place with both WGS84 and NZGD1949 coordinates
-function buildLocationObject(locationName, latLong_WGS84, latLong_NZGD1949)
-{
-    let locationObject = {
-        name: locationName,
-        NZGD1949: latLong_NZGD1949,
-        WGS84: latLong_WGS84
-    };
-    return locationObject;
 }
 
 // interpolate the desired path and necessary parameters into a string
