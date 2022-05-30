5f8202e698bfc828130c48f78ff9f460abcdb338
Crystal Sawers <sawece1@student.op.ac.nz>
2022-05-30 13:36:00 +1200

    convert wind speed to knots

--- js/weatheroutlook.js
+++ js/weatheroutlook.js
@@ -44,11 +44,11 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Dunedin,nz&APPID=a0734a
     //for some reason the max temp and min temp are the same in this api
 
     let speeddiv = document.createElement("div");
-    speeddiv.innerHTML =`${d.wind.speed}` + " m/s";
+    speeddiv.innerHTML = windConverter(`${d.wind.speed}`).toFixed(1) + " kts";
     outlookgrid.appendChild(speeddiv);
 
     let gustdiv = document.createElement("div");
-    gustdiv.innerHTML =`${d.wind.gust}` + " m/s";
+    gustdiv.innerHTML = windConverter(`${d.wind.gust}`). toFixed(1) + " kts";
     outlookgrid.appendChild(gustdiv);
 
     let visibility = document.createElement("div");
@@ -59,9 +59,17 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Dunedin,nz&APPID=a0734a
    console.log(lowtempdiv);
 });
 
-
+//converting temperature from Kelvin to Celsius
 function temperatureConverter(valNum) {
     valNum = parseFloat(valNum);
     valNum = valNum- 273.15 ;
     return valNum;
   } 
+
+  //convertting wind from metres per second to knots
+  function windConverter(toKnots) {
+      toKnots = parseFloat(toKnots);
+      toKnots = toKnots * 1.94384;
+      return toKnots;
+  }
+
