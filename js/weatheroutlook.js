d982dd5f7061166bbdbe9939832a326161f827f6
Crystal Sawers <sawece1@student.op.ac.nz>
2022-06-04 11:48:30 +1200

    fix wind conversion unit and add new api key

--- js/weatheroutlook.js
+++ js/weatheroutlook.js
@@ -1,6 +1,6 @@
 const options = { 
     method: 'GET', 
-    headers: { 'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com', 'X-RapidAPI-Key': '8eb8056ff8msh4ae675fa5ef2a15p16e335jsnf8784edb56a7' } };
+    headers: { 'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com', 'X-RapidAPI-Key': '1965ab000emsh3a33d23ec75627ep1864f5jsn8bc810fd95ac' } };
 
 let body = document.querySelector("body");
 let saturdayGrid = document.querySelector("#saturday");
@@ -34,17 +34,17 @@ fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=-45.8755&
 
     // fetching  and appending the high temperature
     let hightempdiv = document.createElement("div");
-    hightempdiv.innerHTML = `${data.app_max_temp}`  + " °C";
+    hightempdiv.innerHTML = `${data.max_temp}`  + " °C";
     saturdayGrid.appendChild(hightempdiv);
 
     // fetching and appending low temperature
     let lowtempdiv = document.createElement("div");
-    lowtempdiv.innerHTML = `${data.app_min_temp}` + " °C";
+    lowtempdiv.innerHTML = `${data.min_temp}` + " °C";
     saturdayGrid.appendChild(lowtempdiv);
 
 
     let speeddiv = document.createElement("div");
-    speeddiv.innerHTML = windConverter(`${data.wind_spd}`).toFixed(1) + " kts"; //don't know what units they are
+    speeddiv.innerHTML = windConverter(`${data.wind_spd}`).toFixed(1) + " kts"; 
     saturdayGrid.appendChild(speeddiv);
 
     let gustdiv = document.createElement("div");
@@ -74,11 +74,11 @@ fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=-45.8755&
     sundayGrid.appendChild(sunConditions);
 
     let sundayHigh = document.createElement("div");
-    sundayHigh.innerHTML = `${data.app_max_temp}`  + " °C";
+    sundayHigh.innerHTML = `${data.max_temp}`  + " °C";
     sundayGrid.appendChild(sundayHigh);
 
     let sundayLow = document.createElement("div");
-    sundayLow.innerHTML = `${data.app_min_temp}` + " °C";
+    sundayLow.innerHTML = `${data.min_temp}` + " °C";
     sundayGrid.appendChild(sundayLow);
 
     let sundayWind = document.createElement("div");
@@ -93,21 +93,24 @@ fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=-45.8755&
     sundayDir.innerHTML = `${data.wind_cdir}`;
     sundayGrid.appendChild(sundayDir);
 
+
+//response["data"].filter() filter out the saturday and sunday dates
+
 response["data"].sort((a,b) =>{
-//sorting by day/date (for every saturday and sunday)
+//sorting by day/date saturday then sunday
 })
-    response["data"].splice(4); // just testing the splice method in case I need it
+    response["data"].splice(4); // just testing the splice method in case I need it but data isn't filtered or sorted yet
     
     }).console.log(response)).catch(err => console.error(err))
 
 
 
 
-  //converting wind from kilometers per hour to knots
-  function windConverter(kphtoKnots) {
-      kphtoKnots = parseFloat(kphtoKnots);
-      kphtoKnots = kphtoKnots * 0.539957;
-      return kphtoKnots;
+  //converting wind from metres per second to knots
+  function windConverter(mpstoKnots) {
+    mpstoKnots = parseFloat(mpstoKnots);
+    mpstoKnots = mpstoKnots * 1.94384;
+      return mpstoKnots;
   }
 
 
