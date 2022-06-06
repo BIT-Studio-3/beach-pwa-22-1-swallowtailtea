ff33ac55af3d99e5dd6fb3e1c62de63458ea6379
Crystal Sawers <sawece1@student.op.ac.nz>
2022-06-06 15:27:48 +1200

    add date data and merged into one grid

--- js/weatheroutlook.js
+++ js/weatheroutlook.js
@@ -3,12 +3,11 @@ const options = {
     headers: { 'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com', 'X-RapidAPI-Key': '87210298a2mshea45f948e1897a3p1bae45jsn8772730bc595' } };
 
 let body = document.querySelector("body");
-let saturdayGrid = document.querySelector("#saturday");
-
-let sundayGrid = document.querySelector("#sunday");
+let weekendGrid = document.querySelector("#weekendgrid");
 
 
-let titles = ["Conditions", "High", "Low", "Wind Speed", "Wind Gust", "Wind Direction"];
+
+let titles = ["Date", "Conditions", "High", "Low", "Wind Speed", "Wind Gust", "Wind Direction"];
 
 fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=-45.8755&lon=170.50286', options)
 .then(response => response.json())
@@ -25,75 +24,80 @@ fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=-45.8755&
     for (let index = 0; index < titles.length; index++) {
         let div = document.createElement("div");
         div.innerHTML = titles[index];
-        saturdayGrid.appendChild(div);
+        weekendGrid.appendChild(div);
     }
 
+    //date that needs to be formatted to say the day
+
+    let validDate = document.createElement("div");
+    validDate.innerHTML = `${data.valid_date}`;
+    weekendGrid.appendChild(validDate);
     
     // weather conditions, like partly cloudy or patchy rain
     let satConditions = document.createElement("div");
     satConditions.innerHTML = `${data.weather.description}`;
-    saturdayGrid.appendChild(satConditions);
+    weekendGrid.appendChild(satConditions);
 
     // fetching  and appending the high temperature
     let hightempdiv = document.createElement("div");
     hightempdiv.innerHTML = `${data.max_temp}`  + " °C";
-    saturdayGrid.appendChild(hightempdiv);
+    weekendGrid.appendChild(hightempdiv);
 
     // fetching and appending low temperature
     let lowtempdiv = document.createElement("div");
     lowtempdiv.innerHTML = `${data.min_temp}` + " °C";
-    saturdayGrid.appendChild(lowtempdiv);
+    weekendGrid.appendChild(lowtempdiv);
 
 
     let speeddiv = document.createElement("div");
     speeddiv.innerHTML = windConverter(`${data.wind_spd}`).toFixed(1) + " kts"; 
-    saturdayGrid.appendChild(speeddiv);
+    weekendGrid.appendChild(speeddiv);
 
     let gustdiv = document.createElement("div");
     gustdiv.innerHTML = windConverter(`${data.wind_gust_spd}`). toFixed(1) + " kts";
-    saturdayGrid.appendChild(gustdiv);
+    weekendGrid.appendChild(gustdiv);
 
     let windDirection = document.createElement("div");
     windDirection.innerHTML = `${data.wind_cdir}`; 
-    saturdayGrid.appendChild(windDirection);
+    weekendGrid.appendChild(windDirection);
 
 
 
 
 
 
-    //sunday grid
+    // //sunday grid
 
-    for (let index = 0; index < titles.length; index++) {
-        let div = document.createElement("div");
-        div.innerHTML = titles[index];
-        sundayGrid.appendChild(div);
-    }
+    // for (let index = 0; index < titles.length; index++) {
+    //     let div = document.createElement("div");
+    //     div.innerHTML = titles[index];
+    //     sundayGrid.appendChild(div);
+    // }
 
-    // weather conditions, like partly cloudy or patchy rain
-    let sunConditions = document.createElement("div");
-    sunConditions.innerHTML = `${data.weather.description}`;
-    sundayGrid.appendChild(sunConditions);
+    // // weather conditions, like partly cloudy or patchy rain
+    // let sunConditions = document.createElement("div");
+    // sunConditions.innerHTML = `${data.weather.description}`;
+    // sundayGrid.appendChild(sunConditions);
 
-    let sundayHigh = document.createElement("div");
-    sundayHigh.innerHTML = `${data.max_temp}`  + " °C";
-    sundayGrid.appendChild(sundayHigh);
+    // let sundayHigh = document.createElement("div");
+    // sundayHigh.innerHTML = `${data.max_temp}`  + " °C";
+    // sundayGrid.appendChild(sundayHigh);
 
-    let sundayLow = document.createElement("div");
-    sundayLow.innerHTML = `${data.min_temp}` + " °C";
-    sundayGrid.appendChild(sundayLow);
+    // let sundayLow = document.createElement("div");
+    // sundayLow.innerHTML = `${data.min_temp}` + " °C";
+    // sundayGrid.appendChild(sundayLow);
 
-    let sundayWind = document.createElement("div");
-    sundayWind.innerHTML = windConverter(`${data.wind_spd}`).toFixed(1) + " kts";
-    sundayGrid.appendChild(sundayWind);
+    // let sundayWind = document.createElement("div");
+    // sundayWind.innerHTML = windConverter(`${data.wind_spd}`).toFixed(1) + " kts";
+    // sundayGrid.appendChild(sundayWind);
 
-    let sundayGust = document.createElement("div");
-    sundayGust.innerHTML = windConverter(`${data.wind_gust_spd}`). toFixed(1) + " kts";
-    sundayGrid.appendChild(sundayGust);
+    // let sundayGust = document.createElement("div");
+    // sundayGust.innerHTML = windConverter(`${data.wind_gust_spd}`). toFixed(1) + " kts";
+    // sundayGrid.appendChild(sundayGust);
 
-    let sundayDir = document.createElement("div");
-    sundayDir.innerHTML = `${data.wind_cdir}`;
-    sundayGrid.appendChild(sundayDir);
+    // let sundayDir = document.createElement("div");
+    // sundayDir.innerHTML = `${data.wind_cdir}`;
+    // sundayGrid.appendChild(sundayDir);
 
 
 //response["data"].filter() filter out the saturday and sunday dates
