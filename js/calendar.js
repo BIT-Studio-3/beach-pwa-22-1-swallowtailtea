6c87d85bbd2d33c29ce8d267f44e3d639d0723cc
Andrew Davie <DAVIAA2@student.op.ac.nz>
2022-06-08 18:15:42 +1200

    filtered the times to only day

--- js/calendar.js
+++ js/calendar.js
@@ -10,14 +10,15 @@ document.querySelector("body").append(ul);
 fetch(buildNIWA_URL(NIWA_PATHS.data, portChalmers, 30)).then(response => response.json()).then(data =>
 {
 
-    // Adons had a big hand in helping me write this
     let tideInfo = data.values;
-
     let allTides = []
-
+    
     let newDate = 0;
 
+    //let filtered = tideInfo.filter(tideInfo => (tideInfo.time.substring(11,13) >= 6) && (tideInfo.time.substring(11,13) <= 18));
+
     tideInfo.forEach((tide,i) => {
+
         let time = tide.time.replace("Z","")
         let ms = Date.parse(time);
         let date = new Date(ms);
@@ -34,45 +35,20 @@ fetch(buildNIWA_URL(NIWA_PATHS.data, portChalmers, 30)).then(response => respons
     })
 
     Object.entries(allTides).forEach(([k,v]) => {
+        console.log(allTides);
         let li = document.createElement("li");
-
         li.innerHTML = `<b>${v[0].toDateString()}<br/>`;
-
         v.forEach(time => {
-            li.innerHTML += time.getHours() + "<br/>";
+            
+            if((time.getHours() >= 6) && (time.getHours() <=18)){
+                console.log(time)
+                li.innerHTML += (`Time ${time.getHours()} : ${time.getMinutes()} || Tide Height ${time.values}  "<br/>"`);
+            }
         })
-
+        
         ul.append(li);
     })
 
-   // console.log(allTides);
-
-
-  //  let filtered = tideInfo.filter(tideInfo => (tideInfo.time.substring(11,13) >= 6) && (tideInfo.time.substring(11,13) <= 18));
-
-    // let days = [];
-    // let index = 1;
-    // while(tideInfo.length > 1)
-    // {
-    // if (tideInfo[0].time.substring(0,10) == tideInfo[index].time.substring(0,10))
-    // {
-    // index++;
-    // }
-    // else
-    // {
-    // days.push(tideInfo.splice(0, index));
-    // index = 0;
-    // }
-    // }
-    // days = days.concat(tideInfo);
-
-    
-    
-    /*filtered.forEach(e => {
-        let li = document.createElement("li");
-        li.innerHTML = e.time;
-        ul.append(li);
-    });*/
 });
 
 
