da6c11fc7f7f104060a47fdb1ab6236dd2e8215f
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2022-06-10 14:57:34 +1200

    choose calendar location with dropdown

--- js/NIWA_Tides.js
+++ js/NIWA_Tides.js
@@ -42,13 +42,4 @@ function buildNIWA_URL(path = NIWA_PATHS.data, locationObject = locations[0], da
     return urlQuery;
 }
 
-// example request for 7 days of Port Chalmers tide data starting June 4th, 2022. 
-// this info could be used to determine low tide / high tide for this time period
-// fetch(buildNIWA_URL(NIWA_PATHS.data, portChalmers, 7, "2022-06-04")).then(response => response.json()).then(data =>
-//     {
-//         let tideInfo = data.values;
-//         console.log(tideInfo);
-//     });
-
-tide_Calendar(currentLocation);
 
--- js/weatheroutlook.js
+++ js/weatheroutlook.js
@@ -9,71 +9,71 @@ let weekendGrid = document.querySelector("#weekendgrid");
 
 let titles = ["Date", "Conditions", "Chance of rain", "High", "Low", "Wind Speed", "Wind Gust", "Wind Direction"];
 
-// fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=-45.8755&lon=170.50286', options)
-// .then(response => response.json())
-// .then(response => 
+fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=-45.8755&lon=170.50286', options)
+.then(response => response.json())
+.then(response => 
     
     
-//     response["data"].filter(day => day.datetime == formatDate(testSaturday(new Date())) || day.datetime == formatDate(testSunday(new Date()))).forEach(data => {
+    response["data"].filter(day => day.datetime == formatDate(testSaturday(new Date())) || day.datetime == formatDate(testSunday(new Date()))).forEach(data => {
 
-//         console.log(data);
-//         //saturday grid
+        console.log(data);
+        //saturday grid
 
         
 
-//     for (let index = 0; index < titles.length; index++) {
-//         let div = document.createElement("div");
-//         div.innerHTML = titles[index];
-//         weekendGrid.appendChild(div);
-//     }
+    for (let index = 0; index < titles.length; index++) {
+        let div = document.createElement("div");
+        div.innerHTML = titles[index];
+        weekendGrid.appendChild(div);
+    }
 
-//     //date that needs to be formatted to say the day
+    //date that needs to be formatted to say the day
 
-//     let datetime = document.createElement("div");
-//     datetime.innerHTML = `${data.datetime}`;
-//     weekendGrid.appendChild(datetime);
+    let datetime = document.createElement("div");
+    datetime.innerHTML = `${data.datetime}`;
+    weekendGrid.appendChild(datetime);
     
-//     // weather conditions, like partly cloudy or patchy rain
-//     let conditions = document.createElement("div");
-//     conditions.innerHTML = `${data.weather.description}`;
-//     weekendGrid.appendChild(conditions);
+    // weather conditions, like partly cloudy or patchy rain
+    let conditions = document.createElement("div");
+    conditions.innerHTML = `${data.weather.description}`;
+    weekendGrid.appendChild(conditions);
 
-//     //chance of precipitation
-//     let chanceofRain = document.createElement("div");
-//     chanceofRain.innerHTML = `${data.pop}` + " %";
-//     weekendGrid.appendChild(chanceofRain);
+    //chance of precipitation
+    let chanceofRain = document.createElement("div");
+    chanceofRain.innerHTML = `${data.pop}` + " %";
+    weekendGrid.appendChild(chanceofRain);
 
-//     // fetching  and appending the high temperature
-//     let hightempdiv = document.createElement("div");
-//     hightempdiv.innerHTML = `${data.max_temp}`  + " °C";
-//     weekendGrid.appendChild(hightempdiv);
+    // fetching  and appending the high temperature
+    let hightempdiv = document.createElement("div");
+    hightempdiv.innerHTML = `${data.max_temp}`  + " °C";
+    weekendGrid.appendChild(hightempdiv);
 
-//     // fetching and appending low temperature
-//     let lowtempdiv = document.createElement("div");
-//     lowtempdiv.innerHTML = `${data.min_temp}` + " °C";
-//     weekendGrid.appendChild(lowtempdiv);
+    // fetching and appending low temperature
+    let lowtempdiv = document.createElement("div");
+    lowtempdiv.innerHTML = `${data.min_temp}` + " °C";
+    weekendGrid.appendChild(lowtempdiv);
 
 
-//     let speeddiv = document.createElement("div");
-//     speeddiv.innerHTML = windConverter(`${data.wind_spd}`).toFixed(1) + " kts"; 
-//     weekendGrid.appendChild(speeddiv);
+    let speeddiv = document.createElement("div");
+    speeddiv.innerHTML = windConverter(`${data.wind_spd}`).toFixed(1) + " kts"; 
+    weekendGrid.appendChild(speeddiv);
 
-//     let gustdiv = document.createElement("div");
-//     gustdiv.innerHTML = windConverter(`${data.wind_gust_spd}`). toFixed(1) + " kts";
-//     weekendGrid.appendChild(gustdiv);
+    let gustdiv = document.createElement("div");
+    gustdiv.innerHTML = windConverter(`${data.wind_gust_spd}`). toFixed(1) + " kts";
+    weekendGrid.appendChild(gustdiv);
 
-//     let windDirection = document.createElement("div");
-//     windDirection.innerHTML = `${data.wind_cdir}`; 
-//     weekendGrid.appendChild(windDirection);
+    let windDirection = document.createElement("div");
+    windDirection.innerHTML = `${data.wind_cdir}`; 
+    weekendGrid.appendChild(windDirection);
 
 
 
 
     
-//     //filtering out only the upcoming weekend dates
+    //filtering out only the upcoming weekend dates
 
 
-//     }).console.log(response)).catch(err => console.error(err))
+    }).console.log(response)).catch(err => console.error(err))
 
 
 
