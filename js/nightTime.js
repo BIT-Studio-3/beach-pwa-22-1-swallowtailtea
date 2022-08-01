2a9195e9786d5034f129bbe12d9922f31673b348
Jordan Hand <handjw1@student.op.ac.nz>
2022-08-01 16:15:12 +1200

    Time boxes now appearing, attempting to put limits on them

--- js/nightTime.js
+++ js/nightTime.js
@@ -6,21 +6,33 @@ let updatedPmTime;
 
 function adjustTime(){
 
-   let amTimeLabel = document.createElement("label");
+  /* let amTimeLabel = document.createElement("label");
    amTimeLabel.classList.add("change_time");
    amTimeLabel.setAttribute('for', 'amTime');
    document.querySelector("body").append("amTimeLabel");
 
    let amTimeControl = document.createElement("input");
    input.setAttribute('id', 'amTime');
-   input.setAttribute('type', 'time');
+   input.setAttribute('type', 'time');*/
    amTimeControl = document.querySelector('input[type="time"]');
-   amTimeControl.value = defaultAmTime;
+   amTimeControl.value = defaultAmTime; 
 
-     if ((updatedAmTime <=10 && updatedAmTime >= 4) && (updatedPmTime <= 10 && updatedPmTime >= 4)){
+   let amTime = document.getElementById("amTime");
+   let pmTime = document.getElementById("pmTime");
+   let amValue = document.getElementById("amValue");
+   let pmValue = document.getElementById("pmValue");
+
+   amTime.addEventListener("input", function() {
+      amValue.innerText = amTime.value;  
+   }, false);
+   pmTime.addEventListener("input", function() {
+      pmValue.innerText = pmTime.value;  
+   }, false);
+
+    /* if ((updatedAmTime <=10 && updatedAmTime >= 4) && (updatedPmTime <= 10 && updatedPmTime >= 4)){
         //do something        
      }
      else{
         //produce error
-     }
+     } */
 }
