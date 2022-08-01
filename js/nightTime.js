42fde4959cdb5744aab62c7bc2d12340c979b659
Jordan Hand <handjw1@student.op.ac.nz>
2022-08-01 15:57:07 +1200

    Setting attributes and beginning to get a label to appear

--- js/nightTime.js
+++ js/nightTime.js
@@ -1,6 +1,26 @@
-function adjustTime(){
-     let amTime = 6;
-     let pmTime = 6;
+let defaultAmTime = 6;
+let defaultPmTime = 6;
 
-     
+let updatedAmTime;
+let updatedPmTime;
+
+function adjustTime(){
+
+   let amTimeLabel = document.createElement("label");
+   amTimeLabel.classList.add("change_time");
+   amTimeLabel.setAttribute('for', 'amTime');
+   document.querySelector("body").append("amTimeLabel");
+
+   let amTimeControl = document.createElement("input");
+   input.setAttribute('id', 'amTime');
+   input.setAttribute('type', 'time');
+   amTimeControl = document.querySelector('input[type="time"]');
+   amTimeControl.value = defaultAmTime;
+
+     if ((updatedAmTime <=10 && updatedAmTime >= 4) && (updatedPmTime <= 10 && updatedPmTime >= 4)){
+        //do something        
+     }
+     else{
+        //produce error
+     }
 }
