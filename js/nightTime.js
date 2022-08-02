10733f7db35d8eeb62d9c2012e9c22decdef5cbb
Jordan Hand <handjw1@student.op.ac.nz>
2022-08-02 13:38:44 +1200

    simplified html and created attributes through js, value updates are working

--- js/nightTime.js
+++ js/nightTime.js
@@ -8,8 +8,6 @@ function adjustTime(){
 
   /* let amTimeLabel = document.createElement("label");
    amTimeLabel.classList.add("change_time");
-   amTimeLabel.setAttribute('for', 'amTime');
-   document.querySelector("body").append("amTimeLabel");
 
    let amTimeControl = document.createElement("input");
    input.setAttribute('id', 'amTime');
@@ -19,11 +17,26 @@ function adjustTime(){
    let amValue = document.getElementById("amValue");
    let pmValue = document.getElementById("pmValue");
 
+   //Morning Time
+   let amTimeLabel = document.getElementById("amTime");
+   amTimeLabel.setAttribute('value', '06:00');
+   amTimeLabel.setAttribute('min', '04:00');
+   amTimeLabel.setAttribute('max', '10:00');
+   document.querySelector('body').append("amTimeLabel");
+
+   //Night Time
+   let pmTimeLabel = document.getElementById("pmTime");
+   pmTimeLabel.setAttribute('value', '18:00');
+   pmTimeLabel.setAttribute('min', '16:00');
+   pmTimeLabel.setAttribute('max', '22:00');
+   document.querySelector('body').append("pmTimeLabel");
+
    amTime.addEventListener("input", function() {
-      amValue.innerText = amTime.value; 
+      amTime.innerText = amTime.value; 
    }, false);
+
    pmTime.addEventListener("input", function() {
-      pmValue.innerText = pmTime.value;  
+      pmTime.innerText = pmTime.value;  
    }, false);
 
 
