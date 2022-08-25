97f25c0737e69927870af48d78bfcb5b31e1fbcb
Tom <83677440+TomMc608@users.noreply.github.com>
2022-08-25 15:41:10 +1200

    console loged all the data that the client ask for. All i need to do is get it displayed on the html page

--- js/weathertoday.js
+++ js/weathertoday.js
@@ -1,15 +1,17 @@
 const lat = -45.874;
 const lon=170.503
 
-/* fetch(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=c85da2b1e276414da98bb90531c73e60`)
-.then(response => response.json())
-.then(response => {
-    data = response;
-    console.log(data);
-}); */
+fetch("http://api.weatherapi.com/v1/forecast.json?key=0b5fdfe1f70b42f6946232056220106&q=Dunedin&days=7&aqi=no&alerts=yes")
+.then(res => res.json())
+.then(data => { 
+    console.log(data.current);
+    console.log("wind gust is " + data.current.gust_kph + " kph");
+    console.log("The current temp is " + data.current.temp_c + "C");
+    console.log("It feels like " + data.current.feelslike_c + "C");
+    console.log("You have a line of site of " + data.current.vis_km + "km");
+    console.log("The wind is coming in at a " + data.current.wind_degree + " degree angle")
+    console.log("The wind direction is " + data.current.wind_dir)
+    console.log("The wind speed is " + data.current.wind_kph + " kph")
+    console.log("The cloud coverage is " + data.current.cloud + "%")
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=776bfd753a2eea4a1cef8736d0a7a95d`)
-.then(response => {
-    data = response;
-    console.log(data);
-}); 
+})
