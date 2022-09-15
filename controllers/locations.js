20d2b7a2fef5eae4953577993bf0ab3b722ba317
Tom <83677440+TomMc608@users.noreply.github.com>
2022-09-15 13:00:54 +1200

    added the first get request in the controller for locations.

---
+++ controllers/locations.js
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const getLocation = async (req, res) => {
+    try {
+        const { id } = req.prams;
+
+        const location = await prisma.location.findUnique({
+            where: { id: Number(id) }
+        })
+
+        if (!location){
+            return res 
+            .status(200)
+            .json({ msg: `No location with the id: ${id} found`})
+        }
+        return res.json({ data: location})
+    } catch (err) {
+        return res.status(500).json({
+            msg: err.message,
+        })
+    }
+}; 
