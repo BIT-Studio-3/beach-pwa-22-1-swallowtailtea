b318c24cc6ce7a2a0d6b746073ff169650b84d4d
Tom <83677440+TomMc608@users.noreply.github.com>
2022-10-17 15:26:46 +1300

    the calander working with user input for times

--- js/calendar.js
+++ js/calendar.js
@@ -3,7 +3,7 @@ let calendar = document.querySelector(".calendar");
 let month = document.createElement("div");
 
 let today = new Date();
-const month_name = today.toLocaleDateString("default", { month: "long" });
+const month_name = today.toLocaleDateString('default', { month: 'long'});
 month.innerHTML = month_name;
 month.classList.add("div_month");
 
@@ -15,7 +15,8 @@ const day = today.getDate();
 todayDate.innerHTML = day;
 todayDate.classList.add("today_date");
 
-calendar.append(month, inner_grid);
+calendar.append(month,inner_grid);
+
   function getData(form) {
     var formData = new FormData(form);
   
@@ -36,145 +37,90 @@ calendar.append(month, inner_grid);
   document.getElementById("UserTime").addEventListener("submit", function (event) {
     event.preventDefault();
     getData(event.target);
+    call();
   });
   
+//make a funtion called call
+function call(){
 fetch(
-  "https://api.niwa.co.nz/tides/data?lat=-45.878761&long=170.502792&numberOfDays=30&startDate=2022-10-05&interval=10&apikey=xWhbCEFDBpnluLEGBoq5ZtujrcN4ZGRf"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const start = "06:00";
-    const end = "18:00";
+  buildNIWA_URL(NIWA_PATHS.data, currentLocation, 31))
+
+.then(response => response.json())
+.then(data => {
     let lowtidedata = document.createElement("div");
     let hightidedata = document.createElement("div");
     lowtidedata.classList.add("lowtidedata");
 
-    let wantedData = data.values;
-    wantedData.splice(0, 2);
-    wantedData.splice(116);
-    let lowHigh = wantedData.filter((x) => {
-      let wTime = new Date(x.time).getUTCHours();
-      return wTime >= 6 && wTime <= 18;
-    });
-
-    //-----------------------------------------------
-
-    //-------------------------------------------------
-
-    for (let i = 1; i <= 31; i++) {
-      let square2 = document.createElement("div");
-      let square = document.createElement("div");
-      let tidedata1 = document.createElement("div");
-      let tidedata2 = document.createElement("div");
-      let tidedata1Full = document.createElement("div");
-      let tidedata2Full = document.createElement("div");
-      let monthNum = document.createElement("div");
-      tidedata1.classList.add("tidedata");
-      tidedata2.classList.add("tidedata");
-      tidedata1Full.classList.add("tidedata1Full");
-      tidedata2Full.classList.add("tidedata2Full");
-      square2.classList.add("square2");
-      square.classList.add("square");
-      monthNum.classList.add("monthNum");
-      monthNum.innerHTML = `${i}`;
-      //square.innerHTML = `${i}`;
-      square.innerHTML += `<span></span>
+    let wantedData = data.values
+    wantedData.splice(0,2)
+    wantedData.splice(116)
+    console.log(wantedData)
+    let lowHigh = wantedData.filter(x =>{
+        let wTime = new Date(x.time).getUTCHours()
+        return wTime >= 6 && wTime <= 18
+        
+      
+      
+    })
+    console.log(lowHigh)
+    for(let i = 1; i <= 31; i++)
+    {
+        let square2 = document.createElement("div");
+        let square = document.createElement("div");
+        let tidedata1 = document.createElement("div");
+        let tidedata2 = document.createElement("div");
+        let tidedata1Full = document.createElement("div");
+        let tidedata2Full = document.createElement("div");
+        let monthNum = document.createElement("div");
+        tidedata1.classList.add("tidedata");
+        tidedata2.classList.add("tidedata");
+        tidedata1Full.classList.add("tidedata1Full");
+        tidedata2Full.classList.add("tidedata2Full");
+        square2.classList.add("square2");
+        square.classList.add("square");
+        monthNum.classList.add("monthNum");
+        monthNum.innerHTML = `${i}`;
+        square.innerHTML += `<span></span>
         <span></span>
         <span></span>
-        <span></span>`;
+        <span></span>`
+    lowHigh.forEach(d =>{
+    let wDate = new Date(d.time).getUTCDate();
+    let AM_Hour = AM.value;
+    let PM_Hour = PM.value;
+    AM_Hour = ("0" + AM_Hour);
 
-      for (let i = 0; i < data.values.length; i++) {
-        TimeStart = data.values[i].time;
-        HightStart = data.values[i].value;
-        TimeEnd = data.values[i].time;
-          HightEnd = data.values[i].value;
-        if (TimeStart.slice(11, 16) == start) {
-          Date = TimeStart.slice(0, 10);
-          TimeStart = TimeStart.slice(11, 16);
-          tidedata1.innerHTML = `${TimeStart} - ${HightStart}m`;
-          tidedata1Full.innerHTML = `${TimeStart}am - ${HightStart}m`;
-         
+     
+    
+    if(wDate == i+1)
+        {
+            if(tidedata1.innerHTML == "")
+            {
+                
+                tidedata1.innerHTML = `${AM_Hour}: - ${d.value}m`
+                tidedata1Full.innerHTML = `${AM_Hour}:am - ${d.value}m`
+            }
+            else
+            {
+                tidedata2.innerHTML = `${PM_Hour}: - ${d.value}m`
+                tidedata2Full.innerHTML = `${PM_Hour}:pm - ${d.value}m`
+            }
         }
-        if (TimeEnd.slice(11, 16) == end) {
-            Date = TimeEnd.slice(0, 10);
-            TimeEnd = TimeEnd.slice(11, 16);
-             tidedata2.innerHTML = `${TimeEnd} - ${HightEnd}m`;
-          tidedata2Full.innerHTML = `${TimeEnd}pm - ${HightEnd}m`;
-          }
-      }
-      {
-        for (let i = 0; i < data.values.length; i++) {
-          
-          
-        }
-      }
+    })
 
-      if (i < day) {
+    if(i < day)
+    {
         square.classList.add("disabled");
-      }
-      square2.append(tidedata1Full, tidedata2Full);
-      square.append(monthNum, tidedata1, tidedata2, square2);
-      inner_grid.append(square);
     }
-  });
-calendar.append(month, inner_grid);
-
-fetch(
-  "https://api.niwa.co.nz/tides/data?lat=-45.878761&long=170.502792&numberOfDays=30&startDate=2022-10-05&interval=10&apikey=xWhbCEFDBpnluLEGBoq5ZtujrcN4ZGRf"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    let index = 5;
-    /*  console.log(data.values.find((element) => (element = 10)));
-    console.log(
-      "=============================================================================="
-    );
-    console.log(data.values.find((element1) => (element1 = 13)));
-    console.log(
-      "=============================================================================="
-    );
-    console.log(
-      `Using an index of ${index} the item returned is ${data.values.at(index)}`
-    );
-    console.log(
-      "=============================================================================="
-    );
-    console.log(data);
-    console.log(
-      "=============================================================================="
-    );
-    console.log(data.values[2].time); */
-
-    const start = "06:00";
-    const end = "18:00";
-
-    //this for loop get the earlest time that the user sets.
-    for (let i = 0; i < data.values.length; i++) {
-      TimeStart = data.values[i].time;
-      HightStart = data.values[i].value;
-      if (TimeStart.slice(11, 16) == start) {
-        Date = TimeStart.slice(0, 10);
-        TimeStart = TimeStart.slice(11, 16);
-        //  console.log("Date:", Date, "Time:", TimeStart, "Tide Hight", HightStart);
-      }
+    square2.append(tidedata1Full, tidedata2Full)
+    square.append(monthNum,tidedata1,tidedata2, square2);
+    inner_grid.append(square);
     }
 
-    for (let i = 0; i < data.values.length; i++) {
-      TimeEnd = data.values[i].time;
-      HightEnd = data.values[i].value;
-      if (TimeEnd.slice(11, 16) == end) {
-        Date = TimeEnd.slice(0, 10);
-        TimeEnd = TimeEnd.slice(11, 16);
-        //console.log("Date:", Date, "Time:", TimeEnd, "Tide Hight", HightEnd);
-      }
-    }
+})
+}
+calendar.append(month,inner_grid);
 
-    //This works for showing the tide at a specific time. at an interval of 10min
-    //so the idea is to have a loop function looping over the array to find the data that has the requested time from the user.
 
-    // if the user has entered  "i want to see the tides at 6am to 6pm"
-    //the loop will have to look either by using a formula to find what where that requested time is in the array of data.
-    // or the loop can look in to the time variable and look at time im and check it to the user input variable.
-  });
 
 
