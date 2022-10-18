0934887a1af79a27dd5a4b4d5d80fad779ccd7f3
Tom <83677440+TomMc608@users.noreply.github.com>
2022-10-18 14:37:17 +1300

    got the user input working. the issue was the data structure being passed was not in the right formate

--- js/calendar.js
+++ js/calendar.js
@@ -3,7 +3,7 @@ let calendar = document.querySelector(".calendar");
 let month = document.createElement("div");
 
 let today = new Date();
-const month_name = today.toLocaleDateString('default', { month: 'long'});
+const month_name = today.toLocaleDateString("default", { month: "long" });
 month.innerHTML = month_name;
 month.classList.add("div_month");
 
@@ -15,37 +15,47 @@ const day = today.getDate();
 todayDate.innerHTML = day;
 todayDate.classList.add("today_date");
 
-calendar.append(month,inner_grid);
-var AM 
-var Temp
-var PM
+calendar.append(month, inner_grid);
+
+//--------------------------------------------------------------
+
+var AM;
+var PM;
 var e = document.getElementById("UserTimeAM");
 function onChangeAM() {
-  
   var text = e.options[e.selectedIndex].text;
-  console.log(AM, text);
- 
-   Temp = e.value;
-   localStorage.setItem(AM, Temp);
+  // console.log(AM, text);
+  temp = e.value;
+  console.log("hi" + e.value)
+  AM = temp.slice(1, 2);
+  
+ console.log("AM: " + AM);
 }
 e.onchangeAM = onChangeAM;
 onChangeAM();
-localStorage.getItem(AM, Temp);
-console.log(Temp)
+
+
+
+//--------------------------------------------------------------
 
 var e = document.getElementById("UserTimePM");
 function onChangePM() {
-  
   var text = e.options[e.selectedIndex].text;
-  console.log(PM, text);
+
+  // console.log(PM, text);
+  PM = e.value;
   
-   PM = e.value;
-   call();
+  console.log("Cookie PM: " + PM);
 }
 e.onchangePM = onChangePM;
-onChangePM(); 
+onChangePM();
 
-  /*  function getData(form) {
+
+
+
+//--------------------------------------------------------------
+
+/*  function getData(form) {
     var formData = new FormData(form);
   
     for (var pair of formData.entries()) {
@@ -67,92 +77,79 @@ onChangePM();
     getData(event.target);
     call();
   });  */
-  
-
 
 //make a funtion called call that will call the function that will create the calendar
-function call(){
-  
-fetch(
-  buildNIWA_URL(NIWA_PATHS.data, currentLocation, 31))
 
-.then(response => response.json())
-.then(data => {
+fetch(buildNIWA_URL(NIWA_PATHS.data, currentLocation, 31))
+  .then((response) => response.json())
+  .then((data) => {
     let lowtidedata = document.createElement("div");
     let hightidedata = document.createElement("div");
     lowtidedata.classList.add("lowtidedata");
 
-    let wantedData = data.values
-    wantedData.splice(0,2)
-    wantedData.splice(116)
-    console.log(wantedData)
-    let lowHigh = wantedData.filter(x =>{
-        let wTime = new Date(x.time).getUTCHours()
-        return wTime >= 6 && wTime <= 18
-        
-      
-      
-    })
-    console.log(lowHigh)
-    for(let i = 1; i <= 31; i++)
-    {
-        let square2 = document.createElement("div");
-        let square = document.createElement("div");
-        let tidedata1 = document.createElement("div");
-        let tidedata2 = document.createElement("div");
-        let tidedata1Full = document.createElement("div");
-        let tidedata2Full = document.createElement("div");
-        let monthNum = document.createElement("div");
-        tidedata1.classList.add("tidedata");
-        tidedata2.classList.add("tidedata");
-        tidedata1Full.classList.add("tidedata1Full");
-        tidedata2Full.classList.add("tidedata2Full");
-        square2.classList.add("square2");
-        square.classList.add("square");
-        monthNum.classList.add("monthNum");
-        monthNum.innerHTML = `${i}`;
-        square.innerHTML += `<span></span>
+    let wantedData = data.values;
+    wantedData.splice(0, 2);
+    wantedData.splice(116);
+    //console.log(wantedData)
+    let lowHigh = wantedData.filter((x) => {
+    let wTime = new Date(x.time).getUTCHours();
+    let temp = 1
+   // onChangeAM(AM);
+      return wTime >= AM && wTime <= 5;
+    });
+    //console.log(lowHigh)
+    for (let i = 1; i <= 31; i++) {
+      let square2 = document.createElement("div");
+      let square = document.createElement("div");
+      let tidedata1 = document.createElement("div");
+      let tidedata2 = document.createElement("div");
+      let tidedata1Full = document.createElement("div");
+      let tidedata2Full = document.createElement("div");
+      let monthNum = document.createElement("div");
+      tidedata1.classList.add("tidedata");
+      tidedata2.classList.add("tidedata");
+      tidedata1Full.classList.add("tidedata1Full");
+      tidedata2Full.classList.add("tidedata2Full");
+      square2.classList.add("square2");
+      square.classList.add("square");
+      monthNum.classList.add("monthNum");
+      monthNum.innerHTML = `${i}`;
+      square.innerHTML += `<span></span>
         <span></span>
         <span></span>
-        <span></span>`
-        
-    lowHigh.forEach(d =>{
-    let wDate = new Date(d.time).getUTCDate();
-    var AM_Hour = Temp;
-    var PM_Hour = PM;
-    //AM_Hour = ("0" + AM_Hour);
+        <span></span>`;
 
-    
-    
-    if(wDate == i+1)
-        {
-            if(tidedata1.innerHTML == "")
-            {
-                
-                tidedata1.innerHTML = `${AM_Hour}: - ${d.value}m`
-                tidedata1Full.innerHTML = `${AM_Hour}:am - ${d.value}m`
-            }
-            else
-            {
-                tidedata2.innerHTML = `${PM_Hour}: - ${d.value}m`
-                tidedata2Full.innerHTML = `${PM_Hour}:pm - ${d.value}m`
-            }
+      lowHigh.forEach((d) => {
+        let wDate = new Date(d.time).getUTCDate();
+
+        let wHour = new Date(d.time).getUTCHours();
+
+        wHour = ("0" + wHour).slice(-2);
+
+        let wMintue = new Date(d.time).getUTCMinutes();
+
+        wMintue = ("0" + wHour).slice(-2);
+
+        if (wDate == i + 1) {
+          if (tidedata1.innerHTML == "") {
+            tidedata1.innerHTML = `${wHour}:${wMintue} - ${d.value}m`;
+
+            tidedata1Full.innerHTML = `${wHour}:${wMintue}am - ${d.value}m`;
+          } else {
+            tidedata2.innerHTML = `${wHour}:${wMintue} - ${d.value}m`;
+
+            tidedata2Full.innerHTML = `${wHour}:${wMintue}pm - ${d.value}m`;
+          }
         }
-    })
+      });
 
-    if(i < day)
-    {
+      if (i < day) {
         square.classList.add("disabled");
+      }
+      square2.append(tidedata1Full, tidedata2Full);
+      square.append(monthNum, tidedata1, tidedata2, square2);
+      inner_grid.append(square);
     }
-    square2.append(tidedata1Full, tidedata2Full)
-    square.append(monthNum,tidedata1,tidedata2, square2);
-    inner_grid.append(square);
-    }
-  
-})
-}
-calendar.append(month,inner_grid);
+  });
 
-
-
-
+calendar.append(month, inner_grid);
